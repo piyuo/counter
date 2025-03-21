f937ae980fea724e0990ffc1835be0ced5939cfc
piyuo <piyuo.com@gmail.com>
2025-03-21 16:51:49 -0700

    refactor: remove sqflite_darwin dependency and related code from project

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,7 +10,6 @@ import file_selector_macos
 import package_info_plus
 import path_provider_foundation
 import shared_preferences_foundation
-import sqflite_darwin
 import sqlite3_flutter_libs
 import url_launcher_macos
 import vision
@@ -22,7 +21,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
-  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   Sqlite3FlutterLibsPlugin.register(with: registry.registrar(forPlugin: "Sqlite3FlutterLibsPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   VisionPlugin.register(with: registry.registrar(forPlugin: "VisionPlugin"))
