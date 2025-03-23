d0b358c9ff2dead00f34d014b7720438f74d4a24
piyuo <piyuo.com@gmail.com>
2025-03-23 16:56:18 -0700

    refactor: increase image and text sizes in WizardScreen for better visibility

--- lib/wizard/src/wizard_screen.dart
+++ lib/wizard/src/wizard_screen.dart
@@ -72,12 +72,12 @@ class WizardScreen extends StatelessWidget {
                               children: [
                                 Image(
                                   image: AssetImage('assets/icon/icon.png'),
-                                  width: 52,
-                                  height: 52,
+                                  width: 84,
+                                  height: 84,
                                 ),
                                 const SizedBox(height: 8.0),
                                 Text(context.l.product_name,
-                                    style: const TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold)),
+                                    style: const TextStyle(fontSize: 26.0, fontWeight: FontWeight.bold)),
                                 const SizedBox(height: 4.0),
                                 Text(context.l.wizard_screen_desc,
                                     textAlign: TextAlign.center,
