1317da7165f4195324a8f2084a27b554764735a2
piyuo <piyuo.com@gmail.com>
2025-04-13 19:13:30 -0700

    feat: add application category type to Info.plist and update release script for clarity

--- release_macos.sh
+++ release_macos.sh
@@ -1,1 +1,5 @@
-flutter build macos --release --obfuscate --split-debug-info=debug_info
+flutter build macos --release
+# no obfuscate for now, obfuscate make upload to app store more difficult
+#flutter build macos --release --obfuscate --split-debug-info=debug_info
+
+# after run this script, goto macos/Runner.xcworkspace and run the following command Project > Archive,
