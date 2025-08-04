f358e933d8dbb596efb8105437c90c7453894802
piyuo <piyuo.com@gmail.com>
2025-08-04 12:19:50 -0700

    fix: app stuck in loading state when camera access is denied #45

--- lib/wizard/src/project/video_sources.dart
+++ lib/wizard/src/project/video_sources.dart
@@ -116,56 +116,56 @@ List<Widget> buildVideoSources(
     // Webcam
     if (UniversalPlatform.isDesktop)
       CupertinoListTile(
-          leading: videoSourcesProvider.isLoadingWebcam ? CupertinoActivityIndicator() : Icon(CupertinoIcons.videocam),
-          title: Text(context.l.video_sources_webcam),
-          trailing: CupertinoListTileChevron(),
-          onTap: () async {
-            videoSourcesProvider.setLoadingWebcam(true);
-            try {
-              if (!await isWebcamExists(projectProvider)) {
-                return;
-              }
-
-              // add webcam to project
-              if (isAddMode) {
-                final videoProvider = await projectProvider.newVideoToProject(mediaType: vision.SourceType.webcam);
-                if (context.mounted && videoProvider != null) {
-                  Navigator.of(context).pushReplacementNamed(
-                    webcamRoute,
-                    arguments: {
-                      'videoProvider': videoProvider,
-                      'isAddMode': isAddMode,
-                      'previousPageTitle': previousPageTitle,
-                    },
-                  );
-                }
-                return;
-              }
-
-              // create project with webcam
-              await projectProvider.newProject(
-                mediaType: vision.SourceType.webcam,
-                projectId: app.uuid(),
-              );
-              // wait 1 second for webcam to start. webcam tends to return before it's ready
-              await Future.delayed(const Duration(milliseconds: 300));
-              if (context.mounted) {
-                Navigator.of(context).pushNamed(projectRoute);
-              }
-            } finally {
-              videoSourcesProvider.setLoadingWebcam(false);
+        leading: videoSourcesProvider.isLoadingWebcam ? CupertinoActivityIndicator() : Icon(CupertinoIcons.videocam),
+        title: Text(context.l.video_sources_webcam),
+        trailing: CupertinoListTileChevron(),
+        onTap: () async {
+          videoSourcesProvider.setLoadingWebcam(true);
+          try {
+            if (!await isWebcamExists(projectProvider)) {
+              return;
             }
-          }),
+
+            // add webcam to project
+            if (isAddMode) {
+              final videoProvider = await projectProvider.newVideoToProject(mediaType: vision.SourceType.webcam);
+              if (context.mounted && videoProvider != null) {
+                Navigator.of(context).pushReplacementNamed(
+                  webcamRoute,
+                  arguments: {
+                    'videoProvider': videoProvider,
+                    'isAddMode': isAddMode,
+                    'previousPageTitle': previousPageTitle,
+                  },
+                );
+              }
+              return;
+            }
+
+            // create project with webcam
+            await projectProvider.newProject(mediaType: vision.SourceType.webcam, projectId: app.uuid());
+            // wait 1 second for webcam to start. webcam tends to return before it's ready
+            await Future.delayed(const Duration(milliseconds: 300));
+            if (context.mounted) {
+              Navigator.of(context).pushNamed(projectRoute);
+            }
+          } finally {
+            videoSourcesProvider.setLoadingWebcam(false);
+          }
+        },
+      ),
     // Camera
     if (UniversalPlatform.isMobile)
       CupertinoListTile(
         leading: videoSourcesProvider.isLoadingCamera ? CupertinoActivityIndicator() : Icon(CupertinoIcons.camera),
-        title: Text(context.l.video_sources_camera,
-            style: TextStyle(
-              color: projectProvider.project != null && projectProvider.project!.hasCameraInVideos
-                  ? CupertinoColors.systemGrey.resolveFrom(context)
-                  : CupertinoColors.label.resolveFrom(context),
-            )),
+        title: Text(
+          context.l.video_sources_camera,
+          style: TextStyle(
+            color: projectProvider.project != null && projectProvider.project!.hasCameraInVideos
+                ? CupertinoColors.systemGrey.resolveFrom(context)
+                : CupertinoColors.label.resolveFrom(context),
+          ),
+        ),
         trailing: projectProvider.project != null && projectProvider.project!.hasCameraInVideos
             ? null
             : CupertinoListTileChevron(),
@@ -177,31 +177,31 @@ List<Widget> buildVideoSources(
                   videoSourcesProvider.setLoadingCamera(false);
                   return;
                 }
-
+                /* debug, temporarily disable camera permission check
                 if (await havePhoneCameraPermission() == false) {
                   videoSourcesProvider.setLoadingCamera(false);
                   return;
-                }
+                }*/
 
                 try {
                   // add camera to project
                   if (isAddMode) {
                     final videoProvider = await projectProvider.newVideoToProject(mediaType: vision.SourceType.camera);
                     if (context.mounted && videoProvider != null) {
-                      Navigator.of(context).pushReplacementNamed(cameraRoute, arguments: {
-                        'videoProvider': videoProvider,
-                        'isAddMode': isAddMode,
-                        'previousPageTitle': previousPageTitle,
-                      });
+                      Navigator.of(context).pushReplacementNamed(
+                        cameraRoute,
+                        arguments: {
+                          'videoProvider': videoProvider,
+                          'isAddMode': isAddMode,
+                          'previousPageTitle': previousPageTitle,
+                        },
+                      );
                     }
                     return;
                   }
 
                   // create project with camera
-                  await projectProvider.newProject(
-                    mediaType: vision.SourceType.camera,
-                    projectId: app.uuid(),
-                  );
+                  await projectProvider.newProject(mediaType: vision.SourceType.camera, projectId: app.uuid());
                   if (context.mounted) {
                     // first goto start screen then camera screen immediately
                     Navigator.of(context).pushNamed(projectRoute);
@@ -221,26 +221,28 @@ List<Widget> buildVideoSources(
           try {
             // add live stream to project
             if (isAddMode) {
-              await Navigator.of(context).pushNamed(urlRoute, arguments: {
-                'nextRouteBuilder': (url) async {
-                  await projectProvider.newVideoToProject(mediaType: vision.SourceType.liveStream, path: url);
-                  return projectRoute;
-                }
-              });
+              await Navigator.of(context).pushNamed(
+                urlRoute,
+                arguments: {
+                  'nextRouteBuilder': (url) async {
+                    await projectProvider.newVideoToProject(mediaType: vision.SourceType.liveStream, path: url);
+                    return projectRoute;
+                  },
+                },
+              );
               return;
             }
 
             // create project with live stream
-            Navigator.of(context).pushNamed(urlRoute, arguments: {
-              'nextRouteBuilder': (url) async {
-                projectProvider.newProject(
-                  mediaType: vision.SourceType.liveStream,
-                  path: url,
-                  projectId: app.uuid(),
-                );
-                return projectRoute;
-              }
-            });
+            Navigator.of(context).pushNamed(
+              urlRoute,
+              arguments: {
+                'nextRouteBuilder': (url) async {
+                  projectProvider.newProject(mediaType: vision.SourceType.liveStream, path: url, projectId: app.uuid());
+                  return projectRoute;
+                },
+              },
+            );
           } finally {
             videoSourcesProvider.setLoadingLiveStream(false);
           }
@@ -248,84 +250,81 @@ List<Widget> buildVideoSources(
       ),
     // pick video
     CupertinoListTile(
-        leading: videoSourcesProvider.isLoadingFile ? CupertinoActivityIndicator() : Icon(CupertinoIcons.folder),
-        title: Text(context.l.video_sources_file),
-        trailing: CupertinoListTileChevron(),
-        onTap: () async {
-          videoSourcesProvider.setLoadingFile(true);
-          try {
-            final projectId = app.uuid();
-            final filePath = await pickVideo();
-            if (filePath == 'denied') {
-              var status = await Permission.photos.status;
-              if (status.isPermanentlyDenied) {
-                await showCupertinoDialog(
-                  // ignore: use_build_context_synchronously
-                  context: appkit.globalContext,
-                  builder: (context) {
-                    return CupertinoAlertDialog(
-                      title: Text(context.l.video_sources_photos_denied),
-                      content: Text(context.l.video_sources_photos_denied_msg),
-                      actions: [
-                        CupertinoDialogAction(
-                          textStyle: TextStyle(color: CupertinoColors.label.resolveFrom(context)),
-                          onPressed: () => Navigator.pop(context, false),
-                          child: Text(context.l.cancel),
-                        ),
-                        CupertinoDialogAction(
-                          isDefaultAction: true,
-                          child: Text(context.l.video_sources_photos_goto_settings),
-                          onPressed: () async {
-                            openAppSettings();
-                            Navigator.of(context).pop();
-                          },
-                        ),
-                      ],
-                    );
-                  },
-                );
-                // permanently denied, let user go to settings
-                return;
-              }
-              // temporarily denied , just let user to pick again
-              return;
-            }
-
-            if (filePath == null) {
-              return;
-            }
-
-            // add file to project
-            if (isAddMode) {
-              final videoId = projectProvider.getNextVideoId();
-              final newFilePath = await saveFileToAppDirectory(
-                filePath,
-                projectProvider.project!.projectId,
-                videoId,
+      leading: videoSourcesProvider.isLoadingFile ? CupertinoActivityIndicator() : Icon(CupertinoIcons.folder),
+      title: Text(context.l.video_sources_file),
+      trailing: CupertinoListTileChevron(),
+      onTap: () async {
+        videoSourcesProvider.setLoadingFile(true);
+        try {
+          final projectId = app.uuid();
+          final filePath = await pickVideo();
+          if (filePath == 'denied') {
+            var status = await Permission.photos.status;
+            if (status.isPermanentlyDenied) {
+              await showCupertinoDialog(
+                // ignore: use_build_context_synchronously
+                context: appkit.globalContext,
+                builder: (context) {
+                  return CupertinoAlertDialog(
+                    title: Text(context.l.video_sources_photos_denied),
+                    content: Text(context.l.video_sources_photos_denied_msg),
+                    actions: [
+                      CupertinoDialogAction(
+                        textStyle: TextStyle(color: CupertinoColors.label.resolveFrom(context)),
+                        onPressed: () => Navigator.pop(context, false),
+                        child: Text(context.l.cancel),
+                      ),
+                      CupertinoDialogAction(
+                        isDefaultAction: true,
+                        child: Text(context.l.video_sources_photos_goto_settings),
+                        onPressed: () async {
+                          openAppSettings();
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                    ],
+                  );
+                },
               );
-
-              await projectProvider.newVideoToProject(mediaType: vision.SourceType.file, path: newFilePath);
-              if (context.mounted) Navigator.of(context).pop();
+              // permanently denied, let user go to settings
               return;
             }
-
-            // for sandbox safety
-            final videoId = 1;
-            final newFilePath = await saveFileToAppDirectory(filePath, projectId, videoId);
-            // create project with file
-            await projectProvider.newProject(
-              mediaType: vision.SourceType.file,
-              path: newFilePath,
-              projectId: projectId,
-              videoId: videoId,
-            );
-            if (context.mounted) {
-              Navigator.of(context).pushNamed(projectRoute);
-            }
-          } finally {
-            videoSourcesProvider.setLoadingFile(false);
+            // temporarily denied , just let user to pick again
+            return;
           }
-        }),
+
+          if (filePath == null) {
+            return;
+          }
+
+          // add file to project
+          if (isAddMode) {
+            final videoId = projectProvider.getNextVideoId();
+            final newFilePath = await saveFileToAppDirectory(filePath, projectProvider.project!.projectId, videoId);
+
+            await projectProvider.newVideoToProject(mediaType: vision.SourceType.file, path: newFilePath);
+            if (context.mounted) Navigator.of(context).pop();
+            return;
+          }
+
+          // for sandbox safety
+          final videoId = 1;
+          final newFilePath = await saveFileToAppDirectory(filePath, projectId, videoId);
+          // create project with file
+          await projectProvider.newProject(
+            mediaType: vision.SourceType.file,
+            path: newFilePath,
+            projectId: projectId,
+            videoId: videoId,
+          );
+          if (context.mounted) {
+            Navigator.of(context).pushNamed(projectRoute);
+          }
+        } finally {
+          videoSourcesProvider.setLoadingFile(false);
+        }
+      },
+    ),
   ];
 }
 
